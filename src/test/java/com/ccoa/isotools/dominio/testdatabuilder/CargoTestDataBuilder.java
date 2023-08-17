07591367709d5c027cdece620ed6c108d4e24d14
Juan José Cardona Gómez <juanjo-cardona@hotmail.com>
2023-08-17 17:29:04 -0500

    Se incluye las excepciones

--- src/test/java/com/ccoa/isotools/dominio/testdatabuilder/CargoTestDataBuilder.java
+++ src/test/java/com/ccoa/isotools/dominio/testdatabuilder/CargoTestDataBuilder.java
@@ -5,24 +5,4 @@ import com.ccoa.planeacionestrategica.dominio.modelo.Cargo;
 
 public class CargoTestDataBuilder {
 
-    private String nombre;
-    private Area area;
-
-    public CargoTestDataBuilder() {
-
-        this.nombre = "nombre";
-        this.area = Area.of("Tec");
-    }
-
-    public CargoTestDataBuilder conNombre(String nombre) {
-        this.nombre = nombre;
-        return this;
-    }
-
-    public CargoTestDataBuilder conArea(Area area) {
-        this.area = area;
-        return this;
-    }
-
-    public Cargo build() {return Cargo.of(nombre,area);}
 }
